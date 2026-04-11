c2e15e748356da70be9dffefbe64f7b2c79b50ee
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-04-11 20:26:03 +0530

    added get and post route

--- simpleprojects/notesapp/backend/index.js
+++ simpleprojects/notesapp/backend/index.js
@@ -1,7 +1,18 @@
+
 const express=require("express");
+
+
 const mongoose = require("mongoose");
 
+
+//importing the note model to perform CRUD operations on it
+const Note = require("./models/note");
+
 const app=express();
+
+const cors = require("cors");
+app.use(cors());
+
 
 app.use(express.json());
 
@@ -16,6 +27,32 @@ mongoose.connect("mongodb://127.0.0.1:27017/notesapp")
 
 app.get("/",(req,res)=>{
     res.send("server is running");
+});
+
+
+
+//create a new note
+app.post("/add-note",async(req,res)=>{
+      console.log("ADD API HIT 🔥");  
+    const {title,desc} = req.body;
+
+    try{
+        console.log(title, desc);
+        const note= new Note({title,desc});
+        await note.save();
+        res.json({message:"Note added successfully"});
+
+    } catch (err) {
+        res.status(500).json({message:"Error adding note"});
+    }
+   
+
+});
+
+//get all notes
+app.get("/notes", async (req, res) => {
+  const notes = await Note.find();
+  res.json(notes);
 });
 
 app.listen(8080,()=>{
